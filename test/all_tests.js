79baed88e6ea72b1158bff5fc31bdb2ece086609
Eray Hanoglu <e.hanoglu@panates.com>
2018-08-30 12:06:01 +0300

    [*] Updated tests compatible with sqb v3 [*] Updated dependencites

--- test/all_tests.js
+++ test/all_tests.js
@@ -11,37 +11,34 @@ var result;
 
 describe('PostreSQL select queries', function() {
 
-  it('should serialize reserved word', function(done) {
+  it('should serialize reserved word', function() {
     query = sqb.select('comment').from('table1');
     result = query.generate({
       dialect: 'pg',
       prettyPrint: 0
     });
     assert.equal(result.sql, 'select "comment" from table1');
-    done();
   });
 
-  it('should serialize "limit"', function(done) {
+  it('should serialize "limit"', function() {
     query = sqb.select().from('table1').limit(10);
     result = query.generate({
       dialect: 'pg',
       prettyPrint: 0
     });
     assert.equal(result.sql, 'select * from table1 LIMIT 10');
-    done();
   });
 
-  it('should serialize "offset"', function(done) {
+  it('should serialize "offset"', function() {
     query = sqb.select().from('table1').offset(4);
     result = query.generate({
       dialect: 'pg',
       prettyPrint: 0
     });
     assert.equal(result.sql, 'select * from table1 OFFSET 4');
-    done();
   });
 
-  it('should serialize "limit" pretty print', function(done) {
+  it('should serialize "limit" pretty print', function() {
     query = sqb.select().from('table1').limit(10);
     result = query.generate({
       dialect: 'pg',
@@ -50,10 +47,9 @@ describe('PostreSQL select queries', function() {
     assert.equal(result.sql,
         'select * from table1\n' +
         'LIMIT 10');
-    done();
   });
 
-  it('should serialize "offset" pretty print', function(done) {
+  it('should serialize "offset" pretty print', function() {
     query = sqb.select().from('table1').offset(10);
     result = query.generate({
       dialect: 'pg',
@@ -62,10 +58,9 @@ describe('PostreSQL select queries', function() {
     assert.equal(result.sql,
         'select * from table1\n' +
         'OFFSET 10');
-    done();
   });
 
-  it('should serialize "limit/offset"', function(done) {
+  it('should serialize "limit/offset"', function() {
     query = sqb.select()
         .from('table1')
         .offset(4)
@@ -75,10 +70,9 @@ describe('PostreSQL select queries', function() {
       prettyPrint: 0
     });
     assert.equal(result.sql, 'select * from table1 LIMIT 10 OFFSET 4');
-    done();
   });
 
-  it('should serialize "limit/offset" pretty print', function(done) {
+  it('should serialize "limit/offset" pretty print', function() {
     query = sqb.select()
         .from('table1')
         .offset(4)
@@ -90,66 +84,62 @@ describe('PostreSQL select queries', function() {
     assert.equal(result.sql,
         'select * from table1\n' +
         'LIMIT 10 OFFSET 4');
-    done();
   });
 
-  it('Should serialize params', function(done) {
-    query = sqb.select().from('table1').where(Op.eq('ID', /ID/));
+  it('Should serialize params', function() {
+    query = sqb.select().from('table1').where({ID: /ID/});
     result = query.generate({
       dialect: 'pg',
-      prettyPrint: 0
-    }, {ID: 5});
+      prettyPrint: 0,
+      values: {ID: 5}
+    });
     assert.equal(result.sql, 'select * from table1 where ID = $1');
     assert.deepEqual(result.values, [5]);
-    done();
   });
 
-  it('Should serialize array params for "in" operator', function(done) {
+  it('Should serialize array params for "in" operator', function() {
     query = sqb.select().from('table1')
-        .where(Op.in('ID', /id/))
-        .params({id: [1, 2, 3]});
+        .where({'ID in': /id/})
+        .values({id: [1, 2, 3]});
     result = query.generate({
       dialect: 'pg',
       prettyPrint: 0
-    }, {ID: 5});
+    });
     assert.equal(result.sql, 'select * from table1 where ID = ANY($1)');
     assert.deepEqual(result.values, [[1, 2, 3]]);
-    done();
   });
 
-  it('Should serialize array for "in" operator', function(done) {
+  it('Should serialize array for "in" operator', function() {
     query = sqb.select().from('table1')
-        .where(Op.in('ID', [1, 2, 3]));
+        .where({'ID in': [1, 2, 3]});
     result = query.generate({
       dialect: 'pg',
-      prettyPrint: 0
-    }, {ID: 5});
+      prettyPrint: 0,
+      values: {ID: 5}
+    });
     assert.equal(result.sql, 'select * from table1 where ID in (1,2,3)');
-    done();
   });
 
-  it('Should serialize array params for "not in" operator', function(done) {
+  it('Should serialize array params for "not in" operator', function() {
     query = sqb.select().from('table1')
-        .where(Op.notIn('ID', /id/))
-        .params({id: [1, 2, 3]});
+        .where({'ID !in': /id/})
+        .values({id: [1, 2, 3]});
     result = query.generate({
       dialect: 'pg',
       prettyPrint: 0
-    }, {ID: 5});
+    });
     assert.equal(result.sql, 'select * from table1 where ID not = ANY($1)');
     assert.deepEqual(result.values, [[1, 2, 3]]);
-    done();
   });
 
-  it('Should serialize array for "not in" operator', function(done) {
+  it('Should serialize array for "not in" operator', function() {
     query = sqb.select().from('table1')
-        .where(Op.notIn('ID', [1, 2, 3]));
+        .where({'ID !in': [1, 2, 3]});
     result = query.generate({
       dialect: 'pg',
       prettyPrint: 0
-    }, {ID: 5});
+    });
     assert.equal(result.sql, 'select * from table1 where ID not in (1,2,3)');
-    done();
   });
 
 });
